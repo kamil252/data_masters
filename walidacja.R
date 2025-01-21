c982302a95cf0a54ff693fdbfe44daf8c109a458
Nina <nina.gorczynska02@gmail.com>
2025-01-21 19:20:51 +0100

    Poprawienie regul walidacyjnych

--- walidacja.R
+++ walidacja.R
@@ -6,21 +6,18 @@ library(validate)
 reguly_walidacyjne <- validator(
   
   #dochod wnioskodawcy musi byc wiekszy niz 0
-  data$ApplicantIncome > 0,
+  data$ApplicantIncome > 0 | is.na(data$ApplicantIncome),
   
   #kwota pozyczki musi byc wieksza od 0
-  data$LoanAmount > 0,
+  data$LoanAmount > 0 | is.na(data$LoanAmount),
   
   #historia kredytowa powinna byc deklarowana jako 0, 1 lub NA
   data$Credit_History %in% c(0,1,NA),
   
   #plec powinna byc jako 0 lub 1
-  data$Gender %in% c(0,1),
+  data$Gender %in% c(0,1,NA),
   
   #liczba osob na utrzymaniu musi byc liczba calkowita
-  is.numeric(data$Dependents) & data$Dependents >= 0,
-  
-  #jesli ktos jest w zwiazku malzenskim, to ilosc osob na utrzymaniu nie moze byc 0
-  if(data$Married == 1) data$Dependents > 0
+  is.numeric(data$Dependents) & data$Dependents >= 0 | is.na(data$Dependents)
   
 )
