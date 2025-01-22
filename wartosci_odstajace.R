9e0059b5aea87ddfde93c2fdd4f64b9b2bd628c0
Nina <nina.gorczynska02@gmail.com>
2025-01-22 19:35:42 +0100

    Dodanie do pliku histogramow
    Utworzenie histogramow dla zmiennych ApplicantIncome, CoapplicantIncome oraz LoanAmount.

--- wartosci_odstajace.R
+++ wartosci_odstajace.R
@@ -20,4 +20,21 @@ ggplot(data, aes(y = LoanAmount)) +
   geom_boxplot(fill="ivory") +
   labs(title = "Wykres pudełkowy dla Loan Amount")
 
+#Histogramy dla powyzszych zmiennych
+ggplot(data) +
+  aes(x = ApplicantIncome) +
+  geom_histogram(bins = 30, fill = "maroon3") +
+  labs(title = "Histogram dla Applicant Income")+
+  theme_minimal()
 
+ggplot(data) +
+  aes(x = CoapplicantIncome) +
+  geom_histogram(bins = 30, fill = "maroon3") +
+  labs(title = "Histogram dla Copplicant Income") +
+  theme_minimal()
+
+ggplot(data) +
+  aes(x = LoanAmount) +
+  geom_histogram(bins = 30, fill = "maroon3") +
+  labs(title = "Histogram dla Loan Amount") +
+  theme_minimal()
