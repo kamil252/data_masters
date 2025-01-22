d29a57073d752ed78257706f9081a13fe07eb8f0
Nina <nina.gorczynska02@gmail.com>
2025-01-22 19:49:40 +0100

    Zestawienie wizualizacji
    Przypisano nazwy do wykresow oraz wykorzystano funkcje by zwizualizowac wykresy obok siebie

--- wartosci_odstajace.R
+++ wartosci_odstajace.R
@@ -8,33 +8,35 @@ View(clean_data)
 
 
 #Wykresy pudelkowe dla ApplicantIncome, CoapplicantIncome oraz LoanAmount
-ggplot(data, aes(y = ApplicantIncome)) +
+pudelkowyApplicantIncome <- ggplot(data, aes(y = ApplicantIncome)) +
   geom_boxplot(fill="ivory") +
   labs(title = "Wykres pudełkowy dla Applicant Income")
 
-ggplot(data, aes(y = CoapplicantIncome)) +
+pudelkowyCoapplicantIncome <- ggplot(data, aes(y = CoapplicantIncome)) +
   geom_boxplot(fill="ivory") +
   labs(title = "Wykres pudełkowy dla Coapplicant Income")
 
-ggplot(data, aes(y = LoanAmount)) +
+pudelkowyLoanAmount <- ggplot(data, aes(y = LoanAmount)) +
   geom_boxplot(fill="ivory") +
   labs(title = "Wykres pudełkowy dla Loan Amount")
 
 #Histogramy dla powyzszych zmiennych
-ggplot(data) +
+histogramApplicantIncome <- ggplot(data) +
   aes(x = ApplicantIncome) +
   geom_histogram(bins = 30, fill = "maroon3") +
   labs(title = "Histogram dla Applicant Income")+
   theme_minimal()
 
-ggplot(data) +
+histogramCoapplicantIncome <- ggplot(data) +
   aes(x = CoapplicantIncome) +
   geom_histogram(bins = 30, fill = "maroon3") +
   labs(title = "Histogram dla Copplicant Income") +
   theme_minimal()
 
-ggplot(data) +
+histogramLoanAmount <- ggplot(data) +
   aes(x = LoanAmount) +
   geom_histogram(bins = 30, fill = "maroon3") +
   labs(title = "Histogram dla Loan Amount") +
   theme_minimal()
+
+gridExtra::grid.arrange(pudelkowyApplicantIncome, pudelkowyCoapplicantIncome, pudelkowyLoanAmount, histogramApplicantIncome, histogramCoapplicantIncome, histogramLoanAmount, nrow=2)
