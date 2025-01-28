2a0e043e351339dc08cafd7889796945528f7454
Kamil Ciosek <kamil.ciosek98@gmail.com>
2025-01-28 14:46:05 +0100

    Poprawa typow klientow 2

--- podzial_klientow.R
+++ podzial_klientow.R
@@ -6,8 +6,26 @@ data$HouseholdIncome <- data$ApplicantIncome + data$CoapplicantIncome
 # Dochód na osobę zależną 
 data$IncomePerDependent <- ifelse(data$Dependents == 0, data$HouseholdIncome, data$HouseholdIncome / data$Dependents)
 
-# Dochód na osobę zależną w kontekście pożyczki
-data$IncomePerDependentToLoan <- data$IncomePerDependent / data$LoanAmount
+
+identyfikacja <- function(column) {
+  Q1 <- quantile(column, 0.25, na.rm = TRUE)
+  Q3 <- quantile(column, 0.75, na.rm = TRUE)
+  IQR <- Q3 - Q1
+  lower_bound <- Q1 - 1.5 * IQR
+  upper_bound <- Q3 + 1.5 * IQR
+  
+  outliers <- column[column < lower_bound | column > upper_bound]
+  return(list(
+    lower_bound = lower_bound,
+    upper_bound = upper_bound,
+    outliers = outliers
+  ))
+}
+
+odstajace <- identyfikacja(data$IncomePerDependent)
+
+data$type_of_client <- ifelse(data$IncomePerDependent >= min(odstajace$outliers),1,0)
+table(data$type_of_client)
 
 
-print(data)
+write.csv(data, "dataset/types_of_clients_data.csv")
