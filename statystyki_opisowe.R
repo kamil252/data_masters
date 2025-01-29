3f95dbc4a9b51720d555e440749625f72483a53f
Nina <nina.gorczynska02@gmail.com>
2025-01-29 23:20:09 +0100

    Utworzenie statystyk opisowych dla zmiennej IncomePerDependent

--- statystyki_opisowe.R
+++ statystyki_opisowe.R
@@ -1,1 +1,21 @@
+library(readr)
 data <- read_csv("dataset/types_of_clients_data.csv")
+
+
+#Statystyki opisowe
+
+statystyki <-
+  list("IncomePerDependent" =
+         list("Średnia"= ~(mean(IncomePerDependent)),
+              "Mediana"= ~(median(IncomePerDependent)),
+              "Odchylenie standardowe"= ~(sd(IncomePerDependent)),
+              "Pierwszy kwartyl"= ~quantile(IncomePerDependent,0.25),
+              "Trzeci kwartyl"= ~quantile(IncomePerDependent,0.75),
+              "IQR"= ~(IQR(IncomePerDependent)),
+              "Odchylenie ćwiartkowe"= ~(IQR(IncomePerDependent)/2),
+              "Skośność"= ~(skew(IncomePerDependent)),
+              "Kurtoza"= ~(kurtosi(IncomePerDependent)),
+              "Min"= ~min(IncomePerDependent),
+              "Max"= ~max(IncomePerDependent)
+         ))
+
